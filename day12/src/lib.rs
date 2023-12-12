46a7ff6f099769bbd61cf7368568eb2ac95790d0
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2023-12-12 15:03:33 +0100

    day12: prep for rewrite

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -1,50 +1,11 @@
 use aoclib::{geometry::tile::DisplayWidth, parse, CommaSep};
-use arrayvec::ArrayVec;
 use std::{
-    ops::Shl,
     path::Path,
     str::{self, FromStr},
 };
 
-// Actual input has at most 20 conditions, so we can assume that we never have more than 32 unknown elements.
-type Word = u32;
-type Conditions = ArrayVec<Condition, 32>;
-// In a 32 bit word, we cannot possibly have more than 16 distinct groups.
-type DamageGroups = ArrayVec<u8, 16>;
-
-fn get_bit<I>(value: Word, idx: I) -> bool
-where
-    Word: Shl<I, Output = Word>,
-{
-    value & (1 << idx) != 0
-}
-
-fn set_bit<I>(value: Word, idx: I, bit_value: bool) -> Word
-where
-    Word: Shl<I, Output = Word>,
-{
-    if bit_value {
-        value | (1 << idx)
-    } else {
-        value & !(1 << idx)
-    }
-}
-
-/// Compute the actual damage groups from a mapped value
-fn damage_groups(mut mapped_value: Word) -> DamageGroups {
-    let mut found_groups = DamageGroups::new();
-
-    while mapped_value != 0 {
-        // get rid of the trailing 0s, then count the trailing ones
-        mapped_value >>= mapped_value.trailing_zeros();
-        let group = mapped_value.trailing_ones() as _;
-        found_groups.push(group);
-        mapped_value >>= group;
-    }
-
-    found_groups.reverse();
-    found_groups
-}
+type Conditions = Vec<Condition>;
+type DamageGroups = Vec<u8>;
 
 #[derive(
     Debug, Clone, Copy, PartialEq, Eq, parse_display::FromStr, parse_display::Display, strum::EnumIs,
@@ -69,21 +30,31 @@ impl<T> FromStr for Unsep<T>
 where
     T: FromStr + DisplayWidth,
 {
-    type Err = ParseUnspError<<T as FromStr>::Err>;
+    type Err = ParseUnsepError<<T as FromStr>::Err>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let chunker = s.as_bytes().chunks_exact(T::DISPLAY_WIDTH);
         if !chunker.remainder().is_empty() {
-            return Err(ParseUnspError::InputRem);
+            return Err(ParseUnsepError::InputRem);
         }
         let values = chunker
             .map(|chunk| {
                 let s = str::from_utf8(chunk)?;
-                T::from_str(s).map_err(ParseUnspError::Instance)
+                T::from_str(s).map_err(ParseUnsepError::Instance)
             })
             .collect::<Result<Vec<_>, _>>()?;
         Ok(Self(values))
     }
+}
+
+#[derive(Debug, thiserror::Error)]
+enum ParseUnsepError<E> {
+    #[error("parsing an instance")]
+    Instance(#[source] E),
+    #[error("input did not divide neatly by display width")]
+    InputRem,
+    #[error("input did not divide on a utf8 character boundary")]
+    ChunkDivision(#[from] std::str::Utf8Error),
 }
 
 #[derive(Debug, Clone)]
@@ -128,92 +99,53 @@ impl ConditionRecord {
             .count()
     }
 
-    /// Get the complete set of mappings to check.
-    fn possible_mappings(&self) -> impl Iterator<Item = Word> {
-        let unknown_bits = self.n_unknown_bits();
-        let max_value = 1 << unknown_bits;
-        0..max_value
-    }
+    fn unfold(&mut self) {
+        let conditions_len = self.conditions.len();
+        let damage_groups_len = self.damage_groups.len();
 
-    /// Apply the `mapping` value to the unknown bits of `self.conditions`, producing a full condition value.
-    ///
-    /// Note that this proceeds from the least significant bits of the mapping and output. This produces values
-    /// whose bits are in the opposite order (big-endian) from the naive display of the `conditions` vector.
-    fn apply_mapping(&self, mapping: Word) -> Word {
-        let mut mapping_idx = 0;
-        let mut output = 0;
+        for _ in 0..4 {
+            self.conditions.push(Condition::Unknown);
+            self.conditions.extend_from_within(0..conditions_len);
 
-        for (output_idx, condition) in self.conditions.iter().rev().copied().enumerate() {
-            // dbg!(mapping_idx, output_idx, condition);
-            match condition {
-                Condition::Operational => {
-                    // noop; this is a 0 in the output
-                    // (only noop because we know it is initialized to 0)
-                }
-                Condition::Damaged => {
-                    // set a 1 bit in the output
-                    // output |= 1 << output_idx;
-                    output = set_bit(output, output_idx, true)
-                }
-                Condition::Unknown => {
-                    // if the appropriate bit of the mapping is set, set a 1 bit in the output
-                    if get_bit(mapping, mapping_idx) {
-                        output = set_bit(output, output_idx, true)
-                    }
-                    mapping_idx += 1;
-                }
-            }
-            // eprintln!("output:  {output:032b}");
+            self.damage_groups.extend_from_within(0..damage_groups_len);
         }
-
-        // {
-        //     let mut display_conditions = ArrayVec::<u8, 32>::new();
-        //     for condition in self.conditions.iter() {
-        //         display_conditions.push(match condition {
-        //             Condition::Operational => b'0',
-        //             Condition::Damaged => b'1',
-        //             Condition::Unknown => b'?',
-        //         });
-        //     }
-        //     let display_conditions = str::from_utf8(&display_conditions).unwrap();
-        //     eprintln!("display: {display_conditions:>32}");
-        //     eprintln!("output:  {output:032b} ({output:>4}) <- mapping {mapping:016b} ({mapping})");
-        // }
-
-        output
     }
-
-    fn valid_mappings(&self) -> impl '_ + Iterator<Item = Word> {
-        self.possible_mappings()
-            .map(|mapping| self.apply_mapping(mapping))
-            .filter(|&word| damage_groups(word) == self.damage_groups)
-    }
-}
-
-#[derive(Debug, thiserror::Error)]
-enum ParseUnspError<E> {
-    #[error("parsing an instance")]
-    Instance(#[source] E),
-    #[error("input did not divide neatly by display width")]
-    InputRem,
-    #[error("input did not divide on a utf8 character boundary")]
-    ChunkDivision(#[from] std::str::Utf8Error),
 }
 
 // too low: 4264
 pub fn part1(input: &Path) -> Result<(), Error> {
     let records = parse::<ConditionRecord>(input)?.collect::<Vec<_>>();
 
-    let sum_of_valid_mappings = records
-        .into_iter()
-        .map(|record| record.valid_mappings().count())
-        .sum::<usize>();
-    println!("sum of valid mappings (pt 1): {sum_of_valid_mappings}");
+    let sum_of_valid_mappings = todo!();
+    // println!("sum of valid mappings (pt 1): {sum_of_valid_mappings}");
     Ok(())
 }
 
+// ah damnit. had an inkling this would happen. But thought I'd do the dumb thing first, in case it helped.
 pub fn part2(input: &Path) -> Result<(), Error> {
-    unimplemented!("input file: {:?}", input)
+    let records = parse::<ConditionRecord>(input)?
+        .map(|mut record| {
+            record.unfold();
+            record
+        })
+        .collect::<Vec<_>>();
+
+    let max_unknown = records
+        .iter()
+        .map(|record| record.n_unknown_bits())
+        .max()
+        .unwrap();
+    let max_len = records
+        .iter()
+        .map(|record| record.conditions.len())
+        .max()
+        .unwrap();
+
+    println!("unknown: {max_unknown} / max: {max_len}");
+
+    let sum_of_valid_mappings = todo!();
+    // println!("sum of valid mappings (pt 1): {sum_of_valid_mappings}");
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -241,7 +173,7 @@ mod tests {
     fn example_pt1(#[case] condition_record: &str, #[case] expect: usize) {
         dbg!(condition_record);
         let condition_record = condition_record.parse::<ConditionRecord>().unwrap();
-        let mappings = condition_record.valid_mappings().count();
-        assert_eq!(mappings, expect);
+        let mappings = todo!();
+        // assert_eq!(mappings, expect);
     }
 }
